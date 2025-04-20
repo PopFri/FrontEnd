6a8a3183ea747092f229e4703ddb3f670cdd78d4
ohige01 <ohige01@naver.com>
2025-04-20 16:56:18 +0900

    [FEAT] #6 그 시절 영화 추천 구현

--- src/components/home/RecDate.jsx
+++ src/components/home/RecDate.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import backImgsrc from "/images/recSituationBackground.png";
+import "../../styles/home/recDate.css";
+import movieDummy from "../../../public/data/movieDummy";
+import DateSelect from "./DateSelect";
+import MovieList from "./MovieList";
 
 export default function RecDate() {
-  return (
-    <div>RecDate</div>
-  )
+    const [isSubmit, setIsSubmit] = useState(false);
+    const [inputDate, setDate] = useState();
+
+    return (
+        <>
+            {isSubmit ? (
+                <div className="home-recResult">
+                    <div className="recResult-title">
+                        <p className="title-system">그 시절 박스오피스: </p>
+                        <p className="title-user">
+                            {`${inputDate.year}년 ${inputDate.month}월 ${inputDate.week}주차`}
+                        </p>
+                    </div>
+                    <MovieList movieList={movieDummy} />
+                </div>
+            ) : (
+                <div
+                    className="home-recDate"
+                    style={{ backgroundImage: `url(${backImgsrc})` }}
+                >
+                    <p className="recDate-question">
+                        Q. 확인하고 싶은 날짜를 선택해주세요!
+                    </p>
+                    <DateSelect setIsSubmit={setIsSubmit} setDate={setDate} />
+                </div>
+            )}
+        </>
+    );
 }
--- src/components/home/RecSituation.jsx
+++ src/components/home/RecSituation.jsx
@@ -3,6 +3,7 @@ import '../../styles/home/recSituation.css'
 import backImgsrc from '/images/recSituationBackground.png'
 import movieDummy from '../../../public/data/movieDummy'
 import { Link } from 'react-router-dom';
+import MovieList from './MovieList';
 
 export default function RecSituation() {
   const [isSubmit, setIsSubmit] = useState(false);
@@ -24,26 +25,7 @@ export default function RecSituation() {
             {userInput.length > 20 ? userInput.slice(0, 20) + "..." : userInput}
           </p>
         </div>
-        <div className='recResult-movieList'>
-          { 
-            movieDummy.result.map((movie) => {
-              let movieName = movie.name;
-              if(movieName.length > 6)
-                movieName = movieName.slice(0, 6) + "...";
-
-              const movieUrl = `/movie/${movie.id}`;
-
-              return(
-                <Link to={movieUrl} className='movieList-movie'>
-                  <div className='movie-image' style={{backgroundImage: `url(${movie.imageUrl})`}} />
-                  <div className='movie-name'>
-                    <p className='name-text'>{movieName}</p>
-                  </div>
-                </Link>
-              )
-            })
-          }
-        </div>
+        <MovieList movieList = {movieDummy} />
       </div>:
       <div className='home-recSituation' style={{backgroundImage: `url(${backImgsrc})`}}>
         <p className='recSituation-question'>Q. 지금 어떤 상황이신가요?</p>
