838fda18f580779bc7890cd62dd26384e9a13f70
ohige01 <ohige01@naver.com>
2025-04-21 14:38:08 +0900

    [FEAT] #6 날씨별, 시간별 추천 구현

--- src/components/home/RecTime.jsx
+++ src/components/home/RecTime.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import MovieList from '../MovieList'
+import movieDummy from '../../../public/data/movieDummy'
 
 export default function RecTime() {
+  const time = "점심"
+
   return (
-    <div>RecTime</div>
+    <div className='home-recResult'>
+      <div className='recResult-title'>
+        <p className='title-system'>시간별 추천 영화: </p>
+        <p className='title-user'>
+          {time}
+        </p>
+      </div>
+      <MovieList movieList = {movieDummy} />
+    </div>
   )
 }
