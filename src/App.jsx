e04b724e2bfd4f64b4b7ff039b3b29301a98254c
ohige01 <ohige01@naver.com>
2025-05-21 17:12:25 +0900

    [FEAT] matomo volumn 설정

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const instance = createInstance({
   siteId: 1,
   userId: 'UID76903202', // optional, default value: `undefined`.
   trackerUrl: 'http://43.203.83.109:8080/matomo.php', // optional, default value: `${urlBase}matomo.php`
-  srcUrl: 'http://localhost/js/container_XigVTZYK.js', // optional, default value: `${urlBase}matomo.js`
+  srcUrl: 'http://43.203.83.109:80/js/container_XigVTZYK.js', // optional, default value: `${urlBase}matomo.js`
 })
 
 function App() {
