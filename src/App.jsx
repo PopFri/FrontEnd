fd370f390c95a2d4fbf849a700ea6436eb098aca
ohige01 <ohige01@naver.com>
2025-05-21 14:47:17 +0900

    [FEAT] matomo url 변경

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const instance = createInstance({
   siteId: 1,
   userId: 'UID76903202', // optional, default value: `undefined`.
   trackerUrl: 'http://43.203.83.109:8080/matomo.php', // optional, default value: `${urlBase}matomo.php`
-  srcUrl: 'http:/15.164.202.104:80/js/container_XigVTZYK.js', // optional, default value: `${urlBase}matomo.js`
+  srcUrl: 'http://15.164.202.104:80/js/container_XigVTZYK.js', // optional, default value: `${urlBase}matomo.js`
 })
 
 function App() {
