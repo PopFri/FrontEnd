8ce638cedd88456a17f659e6246a06c2213e51a2
penep0 <>
2025-06-05 10:49:35 +0900

    [FEAT] #29 instance 메서드 userId 전송값 변경

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,30 @@ import MyPageHistroy from './pages/MyPageHistroy.jsx';
 import MovieDiscoveryPage from "./pages/MovieDiscoveryPage"
 import Home from './pages/Home.jsx';
 
+const userId = () => {
+  const Server_IP = import.meta.env.VITE_SERVER_IP;
+  return fetch(`${Server_IP}/api/v1/user/provide`, {
+    method: 'GET',
+    credentials: 'include'
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => data.result)
+    .catch(error => {
+      console.error('Error fetching user ID:', error);
+      return undefined;
+    });
+}
+const userProvideId = await userId();
+
 const instance = createInstance({
   urlBase: 'http://14.63.178.153:80',
   siteId: 1,
-  userId: 'UID76903202', // optional, default value: `undefined`.
+  userId: `${userProvideId}`, // optional, default value: `undefined`.
   trackerUrl: 'http://14.63.178.153:80/matomo.php', // optional, default value: `${urlBase}matomo.php`
   srcUrl: 'http://14.63.178.153:80/js/container_hCOAARUa.js', // optional, default value: `${urlBase}matomo.js`
 })
