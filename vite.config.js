135e8f727a1137647ea6a717f090e14f24296348
ohige01 <ohige01@naver.com>
2025-06-11 22:54:54 +0900

    [FIX] 프론트 접근 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    allowedHosts: 'all',
     host: '0.0.0.0',
     port: 5173,
   },
   build: {
-    target: 'es2022' // 또는 'es2022' 도 가능
+    target: 'es2022'
   },
   plugins: [react()],
 })
