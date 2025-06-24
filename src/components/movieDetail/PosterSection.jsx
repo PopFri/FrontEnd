1870f1f933c5d4fa98e6481e78b47849be27739e
penep0 <>
2025-06-24 11:19:33 +0900

    [FEAT] #39 provider 링크 설정

--- src/components/movieDetail/PosterSection.jsx
+++ src/components/movieDetail/PosterSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../styles/movieDetail/PosterSection.css';
+import { Link } from 'react-router-dom';
 
 
 const PosterSection = ({backgroundImageUrl, imageUrl, title, directing, releaseDate, runtime, providers}) => {
@@ -9,7 +10,25 @@ const PosterSection = ({backgroundImageUrl, imageUrl, title, directing, releaseD
         const month = (date.getMonth() + 1).toString().padStart(2, '0');
         const day = date.getDate().toString().padStart(2, '0');
         return `${year}.${month}.${day}`;
-      };
+    };
+
+    const providerLogoUrl = (provider) => {
+        switch (provider.provider_name) {
+            case 'Netflix':
+            case 'Netflix Standard with Ads':
+                return 'https://www.netflix.com';
+            case 'Watcha':
+                return 'https://watcha.com/browse/theater';
+            case 'wavve':
+                return 'https://www.wavve.com';
+            case 'FilmBox+':
+                return 'https://play.filmboxplus.com/';
+            case 'Disney Plus':
+                return 'https://www.disneyplus.com/';
+            default:
+                return null;
+        };
+    };
 
     return (
         <div className="poster-wrapper">
@@ -43,12 +62,19 @@ const PosterSection = ({backgroundImageUrl, imageUrl, title, directing, releaseD
                     {/* 제공업체 */}
                     <div className="provider-container">
                         {providers && providers.map((provider) => (
-                            <img
-                                key={provider.provider_id}
-                                src={`https://image.tmdb.org/t/p/w500${provider.logo_path}`}
-                                alt={provider.provider_name}
-                                className="provider-logo"
-                            />
+                            <a
+                            href={providerLogoUrl(provider)}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="provider-link"
+                            key={provider.provider_id}
+                            >
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w500${provider.logo_path}`}
+                                    alt={provider.provider_name}
+                                    className="provider-logo"
+                                />
+                            </a>
                         ))}
                     </div>
                 </div>
